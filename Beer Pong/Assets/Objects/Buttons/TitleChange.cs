ad687faf87bcf3cda5e862b2deb175fd5c5a16f8
hadesbane <ryandbarth@gmail.com>
2017-12-17 22:45:27 -0600

    Scene Switcher done (with some bugs)!

---
+++ Beer Pong/Assets/Objects/Buttons/TitleChange.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleChange : MonoBehaviour {
+
+    public int button; //button currently "selected"
+
+    // Use this for initialization
+    void Start()
+    {
+        button = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        button = button % 3; //always be on one of 3 buttons
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            button++;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            button--;
+            if (button < 0)
+            {
+                button = 2;
+            }
+        }
+        SwitchStage();
+    }
+
+    public int GetNum()
+    {
+        return button;
+    }
+
+    //checks if a option on screen has been chosen and switches to that if so.
+    void SwitchStage()
+    {
+        if (Input.GetKey(KeyCode.KeypadEnter))
+        {
+            if (button == 0)
+            {
+                SceneManager.LoadScene("Single", LoadSceneMode.Single);
+            }
+            else if (button == 1)
+            {
+                SceneManager.LoadScene("2Player", LoadSceneMode.Single);
+            }
+            else
+            {
+                SceneManager.LoadScene("Options", LoadSceneMode.Single);
+            }
+        }
+    }
+}
